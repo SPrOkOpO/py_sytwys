5500427a462dda7ed6dd1ebf59a45674071d7692
SPrOkOpO <spro@wp.pl>
2020-06-07 20:00:01 +0200

    Synchronizacja 1.6.4

--- dictConstants.py
+++ dictConstants.py
@@ -4,7 +4,7 @@
 # s³ownik sta³ych
 #---------------------------------------------------------------------
 dictConstants = {
-        "NR_WERSJI"             : "1.6.3"
+        "NR_WERSJI"             : "1.6.4"
         ,"GC_DIR_SYTWYS"        : "t:\\sytwys\\"                    
         ,"GC_DIR_LICZNIK"       : "t:\\sytwys\\AAB__licznik\\"      
         }
